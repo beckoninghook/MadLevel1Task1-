fd8600abfc5f89c3fffa8bfd278499809faad982
Niels van Oeffel <beckoninghook@gmail.com>
2020-08-31 18:52:31 +0200

    init

---
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -0,0 +1,14 @@
+package com.example.madlevel1task1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HigherLowerActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_higher_lower)
+    }
+
+
+}
